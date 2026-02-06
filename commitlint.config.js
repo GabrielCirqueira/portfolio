bc8e09501b7bf1bc96f5f71041c410d615268420
GabrielCirqueira <gabrielcirqueira711@gmail.com>
2026-02-05 22:06:58 -0300

    ci(workflows): usar variáveis de ambiente no deploy
    Alteração no deploy.yml para utilizar secrets do GitHub
    para host e key. Ajuste no commitlint para aumentar o
    limite de caracteres do header.

--- commitlint.config.js
+++ commitlint.config.js
@@ -23,6 +23,6 @@ export default {
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'subject-case': [0],
-    'header-max-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 300],
   },
 }
